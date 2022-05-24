48ef373ee0aa5a87435937ec60fa8484bd8fab09
kuklikus <vipkatoxa163@mail.ru>
2022-05-25 02:27:19 +0300

    lab

---
+++ laboratornye/Labs 2,3,4/Assets/script/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody rigidbody;
+    public float force;
+    public float jforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * force * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * force * Time.deltaTime;
+        float y = Input.GetAxis("Jump") * jforce * Time.deltaTime;
+
+        rigidbody.AddForce(x, y, z);
+    }
+}
